c8040f751ff10007ee5f2010f1e6ea104fdcc836
treborsky <rob.koziarski2k@gmail.com>
2023-11-29 01:56:51 +0100

    did some non-work

---
+++ beginner/login_and_registration_system/database_design.md
@@ -0,0 +1,5 @@
+# Login and Registration System - Database Design
+
+The database will hold user data, so login and password.
+
+I don't care, it's a .csv file for now. Whatever.
--- beginner/login_and_registration_system/src/main.cc
+++ beginner/login_and_registration_system/src/main.cc
@@ -8,87 +8,52 @@
  */
 #include <iostream>
 #include <string>
-#include <filesystem>
+#include <vector>
 #include <fstream>
 #include <cstdlib>
 #include <memory>
+#include <optional>
 
-namespace fs = std::filesystem;
+struct Record {
+    std::string username;
+    std::string password;
+};
 
-typedef std::string Login;
-typedef std::string Password;
+class Database {
+  std::vector<Record> database;
+  bool loaded;
 
-class FileHandler {
-public:
-    FileHandler() {
-        const std::string home_dir = std::getenv("HOME");
+ public:
+  Database() {}
 
-        fs::path path_to_database_dir{home_dir.c_str()};
-        path_to_database_dir /= ".login_and_registration";
-        if (not fs::exists(path_to_database_dir)) {
-            fs::create_directory(path_to_database_dir);
-            fs::permissions(path_to_database_dir, fs::perms::all);
-        }
-        path_to_database = fs::path{home_dir} / DATABASE_FILE_PATH_EXT;
-        std::cout << "Database directory: " << path_to_database << std::endl;
+  void Init(const std::string& databaseFilePath) {
+    /* TODO: actual work X D */
+  }
+  
+  bool CheckPassword(const std::string& usernameInput, const std::string& passwordInput) const {
+    const auto passwordFromDatabase = GetPasswordForUsername(usernameInput);
+    if (!passwordFromDatabase.has_value()) {
+        return false;
     }
-
-    ~FileHandler() {
-        CloseFile();
+    else {
+        return passwordInput == passwordFromDatabase.value();
     }
+  }
 
-    const fs::path GetPath() const {
-        return path_to_database;
-    }
-
-    std::fstream& GetFileHandle() {
-        if (not database_file.is_open()) {
-            database_file.open(path_to_database, std::ios::in | std::ios::out);
-        }
-        return database_file;
-    }
-
-    void CloseFile() {
-        if (database_file.is_open()) {
-            database_file.close();
+ private:
+  std::optional<const std::string> GetPasswordForUsername(const std::string& usernameInput) const {
+    std::optional<const std::string> matchingPassword;
+    for (const auto& record : database) {
+        if (record.username == usernameInput) {
+            matchingPassword.emplace(record.password);
         }
     }
-
-private:
-    static constexpr auto DATABASE_FILE_PATH_EXT{".login_and_registration/database.db"};
-    fs::path path_to_database;
-    std::fstream database_file;
+    return matchingPassword;
+  }
 
 };
 
-class Handler {
-public:
-    Handler(std::unique_ptr<FileHandler> database_file_handler) 
-        : db_file_handler{std::move(database_file_handler)} {}
-
-    void Write(const std::string& data) {
-        db_file_handler->GetFileHandle() << data;
-        db_file_handler->CloseFile();
-    }
-
-    std::string Read() const {
-        auto& db = db_file_handler->GetFileHandle();
-        std::string data;
-        db >> data;
-        std::cout << "data: " << data << std::endl;
-        db_file_handler->CloseFile();
-        return data;
-    }
-
-private:
-    std::unique_ptr<FileHandler> db_file_handler;
-
-};
 
 int main() {
-    auto file_handler = std::make_unique<FileHandler>();
-    auto handler = Handler{std::move(file_handler)};
 
-    handler.Write("Hello World");
-    std::cout << handler.Read() << std::endl;
 }
